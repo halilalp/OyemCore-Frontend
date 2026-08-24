32888901e294c81a909b4e0c157bcc2941b0c255
Halil Alp Çalışan <halilalp@oyemsoft.com>
2026-08-24 17:37:38 +0300

    fix: make Podfile post_install signing workaround more robust by appending block

--- packages/mobile/plugins/withPodfilePostInstall.js
+++ packages/mobile/plugins/withPodfilePostInstall.js
@@ -9,29 +9,24 @@ const withPodfilePostInstall = (config) => {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let contents = fs.readFileSync(podfilePath, 'utf-8');
       
-      const targetString = 'post_install do |installer|';
-      const replacementString = `post_install do |installer|
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
-      config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
-      config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
-    end
-  end
+      const patch = `
+# Workaround for Xcode 14+ resource bundle signing error
+post_install do |installer|
   installer.target_installation_results.pod_target_installation_results.each do |pod_name, target_installation_result|
     target_installation_result.resource_bundle_targets.each do |resource_bundle_target|
       resource_bundle_target.build_configurations.each do |config|
         config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
         config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
-        config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
       end
     end
-  end`;
+  end
+end
+`;
 
-      if (contents.includes(targetString) && !contents.includes("resource_bundle_targets")) {
-        contents = contents.replace(targetString, replacementString);
+      if (!contents.includes("resource_bundle_targets")) {
+        contents += patch;
         fs.writeFileSync(podfilePath, contents, 'utf-8');
-        console.log('Successfully injected CODE_SIGNING_ALLOWED = NO workaround into Podfile');
+        console.log('Successfully appended resource bundle signing workaround to Podfile');
       }
       return config;
     },
