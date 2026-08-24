253787ee5c369217910386f073078e8b2610cdcf
Halil Alp Çalışan <halilalp@oyemsoft.com>
2026-08-24 18:07:37 +0300

    fix: disable CODE_SIGNING_ALLOWED for CocoaPods resource bundle targets

--- packages/mobile/plugins/withPodfilePostInstall.js
+++ packages/mobile/plugins/withPodfilePostInstall.js
@@ -22,15 +22,18 @@ const withPodfilePostInstall = (config) => {
 
       const replacementString = `post_install do |installer|
   installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['DEVELOPMENT_TEAM'] = 'NY3T5SAC9V'
+    if target.respond_to?(:product_type) and target.product_type == "com.apple.product-type.bundle"
+      target.build_configurations.each do |config|
+        config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
+        config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
+      end
     end
   end`;
 
-      if (!contents.includes("NY3T5SAC9V")) {
+      if (!contents.includes("product-type.bundle")) {
         contents = contents.replace(targetRegex, replacementString);
         fs.writeFileSync(podfilePath, contents, 'utf-8');
-        console.log('Successfully injected DEVELOPMENT_TEAM workaround into Podfile');
+        console.log('Successfully injected resource bundle signing workaround into Podfile');
       } else {
         console.log('Workaround already injected, skipping.');
       }
